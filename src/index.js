0cbdfaac4de2edb9e54bdea5a094de51c7bb0949
Ivankirsd <ivankirsd@gmail.com>
2018-09-22 02:15:43 +0300

    Upload result

--- src/index.js
+++ src/index.js
@@ -1,26 +1,96 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.expression = [];
+    this.expression.push(initialValue);
   }
 
   add(number) {
-    // your implementation
+    this.expression.push('+');
+    this.expression.push(number);
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.expression.push('-');
+    this.expression.push(number);
+
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.expression.push('*');
+    this.expression.push(number);
+
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.expression.push('/');
+    this.expression.push(number);
+
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.expression.push('^');
+    this.expression.push(number);
+
+    return this;
+  }
+
+  getResult() {
+    let localExpression = [...this.expression];
+
+    if ( localExpression.length > 2) {
+      for (let i = localExpression.length - 1; i > -1 ; i--) {
+        if (localExpression[i] == '^') {
+          localExpression.splice(i - 1, 3, Math.pow(localExpression[i - 1], localExpression[i + 1]));
+          i = localExpression.length - 1;
+        }         
+      }
+    }
+
+    if ( localExpression.length > 2) {
+      for (let i = 0; i < localExpression.length; i++) {
+        if (localExpression[i] == '*') {
+          localExpression.splice(i - 1, 3, localExpression[i - 1] * localExpression[i + 1]);
+          i = 0;
+        }         
+      }
+    }
+
+    if ( localExpression.length > 2) {
+      for (let i = 0; i < localExpression.length; i++) {
+        if (localExpression[i] == '/') {
+          localExpression.splice(i - 1, 3, localExpression[i - 1] / localExpression[i + 1]);
+          i = 0;
+        }         
+      }
+    }
+
+    if ( localExpression.length > 2) {
+      for (let i = 0; i < localExpression.length; i++) {
+        if (localExpression[i] == '-') {
+          localExpression.splice(i - 1, 3, localExpression[i - 1] - localExpression[i + 1]);
+          i = 0;
+        }         
+      }
+    }
+
+    if ( localExpression.length > 2) {
+      for (let i = 0; i < localExpression.length; i++) {
+        if (localExpression[i] == '+') {
+          localExpression.splice(i - 1, 3, localExpression[i - 1] + localExpression[i + 1]);
+          i = 0;
+        }         
+      }
+    }
+    
+    return localExpression[0];
+  }
+
+  valueOf() {
+    return +this.getResult();
   }
 }
 
